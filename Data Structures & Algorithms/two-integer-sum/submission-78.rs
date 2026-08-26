69ff82340c3edf4b5fd9d18604e89ae1367ebc0a
Kushal Gaywala <contact@kushalgaywala.com>
2026-08-26 15:58:37 +0200

    Add: two-integer-sum - submission-78

---
+++ Data Structures & Algorithms/two-integer-sum/submission-78.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut diffs: HashMap<i32, i32> = HashMap::new();
+        
+        for (i, n) in nums.iter().enumerate() {
+            let mut diff = target - n;
+            if diffs.contains_key(&diff) {
+                return vec![diffs[&diff], i as i32];
+            }
+            diffs.insert(*n, i as i32);
+        }
+
+        return vec![1,1]
+    }
+}
