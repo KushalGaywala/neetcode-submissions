43896ed104cd3e25e61e5209011abaf3bca2f790
Kushal Gaywala <contact@kushalgaywala.com>
2026-08-28 16:39:37 +0200

    Add: anagram-groups - submission-35

---
+++ Data Structures & Algorithms/anagram-groups/submission-35.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = []
+        matches = {}
+        
+        for i, string in enumerate(strs):
+            char_count = [0]*26            
+            for j, char in enumerate(string):
+                char_count[ord(char)-97] = char_count[ord(char)-97] + 1
+        
+            if tuple(char_count) not in matches:
+                matches[tuple(char_count)] = [string]
+            else:
+                matches[tuple(char_count)].append(string)
+        
+        for value in matches.values():
+            anagrams.append(list(value))
+
+        return anagrams
