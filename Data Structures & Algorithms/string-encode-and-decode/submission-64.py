8db341be8542ff6c8f7cf7de1cf52b6eddb533a9
Kushal Gaywala <contact@kushalgaywala.com>
2026-08-29 13:57:57 +0200

    Add: string-encode-and-decode - submission-64

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-64.py
@@ -0,0 +1,36 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        enc_str = ""
+        for string in strs:
+            enc_str += f'{str(len(string))}#{string}'
+        return enc_str
+
+    def decode(self, s: str) -> List[str]:
+        str_list = list()
+        char_count = 0
+        str_char_count = ""
+        temp_str = ""
+        i = 0
+        while i < len(s):
+            if s[i] == "#":
+                i += 1
+                char_count = i + int(str_char_count)
+                str_char_count = ""
+                temp_str = ""
+                
+                if i == char_count:
+                    str_list.append("")
+
+                while i < char_count and i < len(s):
+                    temp_str += s[i]
+                    i += 1
+                
+                    if i == char_count:
+                        str_list.append(temp_str)
+
+            else:
+                str_char_count += s[i]
+                i += 1
+        
+        return str_list
